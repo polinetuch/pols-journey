adbecf441abd1d04c175e3c89cb05b916aa8df54
Polinet <uchpolinet@gmail.com>
2020-10-15 14:57:14 +1100

    created react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Poli's Portfolio v3.0</h1>
+    </div>
+  );
+}
+
+export default App;
