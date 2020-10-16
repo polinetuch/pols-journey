cabc70a7769a57a73a9414a5eeaf44a4b41851b7
Polinet <uchpolinet@gmail.com>
2020-10-16 13:21:46 +1100

    added navigation lists to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,10 +4,19 @@ import './Footer.scss';
 const Footer = () => {
     return (
         <div className='footer__container'>
+            <div className='footer__navigation'>
+                <ul className='footer__navigation-list'>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/'>About</a></li>
+                    <li><a href='/'>Skills</a></li>
+                    <li><a href='/'>Projects</a></li>
+                    <li><a href='/'>Contact</a></li>
+                </ul>
+            </div>
             <div className='footer__social-media'>
                 <a href='https://www.linkedin.com/in/polinetuch/'>
                     <img 
-                        className='footer__linkedin'
+                        className='footer__icon'
                         src='https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/linkedin-512.png'
                         alt='linked-in'
                     />
@@ -15,7 +24,7 @@ const Footer = () => {
 
                 <a href='https://github.com/polinetuch'>
                     <img 
-                        className='footer__github'
+                        className='footer__icon'
                         src='https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png'
                         alt='github'
                     />
