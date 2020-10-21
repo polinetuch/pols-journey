552a40add3171f04936f11de8d8d76a95d92c7c3
Polinet <uchpolinet@gmail.com>
2020-10-21 23:29:26 +1100

    added id to Contact for route testing

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -4,7 +4,7 @@ import { FcCellPhone, FcFeedback } from 'react-icons/fc';
 
 const Contact = () => {
     return (
-        <div className='contact'>
+        <div className='contact' id='contact'>
             <ul className='contact__lists'>
                 <li><FcCellPhone className='contact__icon'/> 0423 833 438</li>
                 <li><FcFeedback className='contact__icon'/> uchpolinet@gmail.com</li>
