be0c03f46dd68a86df33412769b9e7d223608b40
Polinet <uchpolinet@gmail.com>
2020-10-29 16:50:14 +1100

    removed anime tag

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,61 +1,6 @@
 import React from 'react';
 import './LandingPage.scss';
-// import {anime} from 'react-anime';
 const LandingPage = () => {
-
-    // const animationRef = React.useRef(null);
-    // React.useEffect(() => {
-    //     animationRef.current = anime({loop: true})
-    //     .add({
-    //       targets: '.ml8 .circle-white',
-    //       scale: [0, 3],
-    //       opacity: [1, 0],
-    //       easing: "easeInOutExpo",
-    //       rotateZ: 360,
-    //       duration: 1100
-    //     }).add({
-    //       targets: '.ml8 .circle-container',
-    //       scale: [0, 1],
-    //       duration: 1100,
-    //       easing: "easeInOutExpo",
-    //       offset: '-=1000'
-    //     }).add({
-    //       targets: '.ml8 .circle-dark',
-    //       scale: [0, 1],
-    //       duration: 1100,
-    //       easing: "easeOutExpo",
-    //       offset: '-=600'
-    //     }).add({
-    //       targets: '.ml8 .letters-left',
-    //       scale: [0, 1],
-    //       duration: 1200,
-    //       offset: '-=550'
-    //     }).add({
-    //       targets: '.ml8 .bang',
-    //       scale: [0, 1],
-    //       rotateZ: [45, 15],
-    //       duration: 1200,
-    //       offset: '-=1000'
-    //     }).add({
-    //       targets: '.ml8',
-    //       opacity: 0,
-    //       duration: 1000,
-    //       easing: "easeOutExpo",
-    //       delay: 1400
-    //     });
-      
-    //   anime({
-    //     targets: '.ml8 .circle-dark-dashed',
-    //     rotateZ: 360,
-    //     duration: 8000,
-    //     easing: "linear",
-    //     loop: true
-    //   });
-      
-    // })
-    
-
-
     return (
         <div className='landing-page__container' id='home'>
             <div  className='landing-page__image'>
@@ -68,17 +13,7 @@ const LandingPage = () => {
                 </figure>
             </div>
             <div className='landing-page__about-me'>
-                <h1 className='ml8'>
-                    <span className='letters-container'>
-                        <span className='letters letters-left'>Hi</span>
-                        <span className='letters bang'>!</span>
-                    </span>
-
-                    <span className='circle cirlce-white'></span>
-                    <span className='circle cirlce-dark'></span>
-                    <span className='circle cirlce-container'></span>
-                
-                </h1>
+                <h1>Hi, I'm <strong>Polinet Uch.</strong></h1>
                 <p>
                     I am aspiring full stack developer. I love building and finding ways
                     to make things easier with technology.
