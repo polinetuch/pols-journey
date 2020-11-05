941e91d0978456616f47f27bb50792c39ec22898
Polinet <uchpolinet@gmail.com>
2020-11-05 12:50:47 +1100

    added budget page

---
+++ src/components/Projects/ProjectPages/BudgetApp.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './project-style.scss';
+import { VscGithub } from 'react-icons/vsc';
+import {BiLinkExternal} from 'react-icons/bi';
+
+const BudgetApp = () => {
+    return (
+        <div className='project'>
+            <h1>Budget App</h1>
+            <hr/>
+            <div className='project__about'>
+                <div>
+                    <h2>About</h2>
+                    <p>Keep track of your incomes and expenses.
+                    </p>
+                </div>
+
+                <div className='line'></div>
+
+                <div>
+                    <h2>Technologies Used</h2>
+                    <ul>
+                        <li>Javascript</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                    </ul>
+                </div>
+
+                <div className='line'></div>
+
+                <div className='project__links'>
+                    <h2>Links</h2>
+                    <ul>
+                        <li><a href='https://github.com/polinetuch/budget-app'><VscGithub/> View On Github</a></li>
+                        <li><a href='https://polinetuch.github.io/budget-app/'><BiLinkExternal/>Try Here</a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr/>
+        </div>
+    )
+}
+
+export default BudgetApp;
