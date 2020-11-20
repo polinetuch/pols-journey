c1ba057a8f469a7b75c9f7622228c0a31b5b6559
Polinet <uchpolinet@gmail.com>
2020-11-20 18:17:01 +1100

    added styling and bounce effect

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import './LandingPage.scss';
+import Bounce from 'react-reveal/Bounce';
 
 const LandingPage = () => {
     return (
         <div className='landing-page__container' id='home'>
-            <div  className='landing-page__image'>
-                <figure>
-                    <img 
-                        className='landing-page__photo'
-                        alt='Cartoon Of Poli'
-                        src='https://github.com/polinetuch/portfolio_v2.0/blob/master/assets/img/polinetuch.jpg?raw=true'
-                    />
-                </figure>
+            <div>
+                <Bounce left>
+                    <figure>
+                        <div className='landing-page__pink'>Poli</div>
+                        <img 
+                            className='landing-page__photo'
+                            alt='Cartoon Of Poli'
+                            src='https://github.com/polinetuch/portfolio_v2.0/blob/master/assets/img/polinetuch.jpg?raw=true'
+                        />
+                        <div className='landing-page__green'></div>
+                        <div className='landing-page__blue'></div>
+                    </figure>
+                </Bounce>
             </div>
             <div className='landing-page__about-me'>
-                <h1>Hello from <strong>Polinet Uch.</strong></h1>
+                <Bounce top>
+                    <h1>Hello from <strong>Polinet Uch.</strong></h1>
+                </Bounce>
                 <hr/>
-                <p>
-                    I am aspiring full stack developer. I love building and finding ways
-                    to make things easier with technology.
-                </p>        
-                <p>
-                When I am not coding, you'll probably find me in my art and craft corner,
-                reading more books or learning new cooking recipes.
-                </p>
+                <Bounce right>
+                    <p>
+                        I am aspiring full stack developer. I love building and finding ways
+                        to make things easier with technology.
+                    </p>        
+                    <p>
+                    When I am not coding, you'll probably find me in my art and craft corner,
+                    reading more books or learning new cooking recipes.
+                    </p>
+                </Bounce>
             </div>
         </div>
     )
